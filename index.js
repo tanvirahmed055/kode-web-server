f450c888aff9610fff5921e5991d9500daf4d4d5
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-12-17 17:05:32 +0600

    made connection with database

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
 const app = express()
-const port = 5000
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oa9tu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+client.connect(err => {
+    const collection = client.db("kodeWeb_Db").collection("services");
+    // perform actions on the collection object
+    console.log(uri);
+    console.log("connected to database");
+});
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to KodeWeb Server!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`KodeWeb Server app listening at http://localhost:${port}`)
 }) 
